504d6456078cebb56d7dfb2b467c0e00ce2843f0
Hans Rip <auxzambia@gmail.com>
2010-04-27 23:25:30 +0200

    initial version

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'C:\\dev\\sf_project\\lib\\vendor\\symfony\\lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+	public function setup()
+	{
+	  $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ *
+ * @package    transzam
+ * @subpackage form
+ * @author     sf_project2
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
